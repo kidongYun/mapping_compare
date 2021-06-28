0cda50db935aa8756164d1899139fa88b704979a
Kidong Yun <oriondigestive@gmail.com>
2021-06-28 11:44:05 +0900

    INIT

---
+++ src/main/java/com/kianyun/mapping_compare/MappingCompareApplication.java
@@ -0,0 +1,13 @@
+package com.kianyun.mapping_compare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingCompareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MappingCompareApplication.class, args);
+    }
+
+}
